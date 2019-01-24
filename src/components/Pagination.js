8453fcf1ae86e4f1539d0148e4eaefdb14835a3b
andi hakim arif <andi.arif@stptower.com>
2019-01-24 14:18:55 +0700

    add react router dom. onclick pagination change url with params

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,15 +3,21 @@ import { connect } from "react-redux";
 
 import { changePage } from "../redux/actions";
 
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
 const Pagination = ({ pages, dispatch }) => {
   // console.log(pages);
   return (
     <React.Fragment>
       {pages &&
         pages.map(page => (
-          <button key={page} onClick={() => dispatch(changePage(page))}>
-            {page}
-          </button>
+          <Router>
+            <button key={page} onClick={() => dispatch(changePage(page))}>
+              <Link to={{ pathname: "/", search: `?page=${page}` }}>
+                {page}
+              </Link>
+            </button>
+          </Router>
         ))}
     </React.Fragment>
   );
